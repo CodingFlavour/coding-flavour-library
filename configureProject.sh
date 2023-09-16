030b673870b01e1b6f28c4a2b598c9e1cb367e1c
DSanchez <danielsanchezpro@gmail.com>
2023-09-16 20:33:17 +0200

    @V1.2 @Helper file + New NextJS architechture + New Sass arch

--- configureProject.sh
+++ configureProject.sh
@@ -3,32 +3,7 @@ PROJECT_NAME=$1
 ERROR_UPPERCASE="Error: Component name must start with a lowercase letter."
 FULLSCRIPTPATH="${BASH_SOURCE[0]}"
 SCRIPTPATH=$(dirname "$FULLSCRIPTPATH")
-
-print_colored_message() {
-    local message=$1
-    local color=$2
-
-    case $color in
-    "red")
-        echo -e "\e[1;31m$message\e[0m"
-        ;;
-    "green")
-        echo -e "\e[1;32m$message\e[0m"
-        ;;
-    "blue")
-        echo -e "\e[1;34m$message\e[0m"
-        ;;
-    "yellow")
-        echo -e "\e[1;33m$message\e[0m"
-        ;;
-    "purple")
-        echo -e "\e[1;35m$message\e[0m"
-        ;;
-    *)
-        echo -e "$message"
-        ;;
-    esac
-}
+source $SCRIPTPATH/helper.sh
 
 show_help() {
     print_colored_message "---------------" "green"
@@ -69,15 +44,31 @@ install_dependencies() {
 
 move_styles() {
     print_colored_message "Starting to move library files" green
-    mkdir ./$PROJECT_NAME/src/styles
-    cp $SCRIPTPATH/styles/grid-system.scss ./$PROJECT_NAME/src/styles/grid-system.scss
+    cp $SCRIPTPATH/styles/main.scss ./$PROJECT_NAME/src/presentation/styles/main.scss
+    cp $SCRIPTPATH/styles/base/grid-system.scss ./$PROJECT_NAME/src/presentation/styles/base/grid-system.scss
     print_colored_message "Ended moving library files" green
+}
+
+create_root_dir_front() {
+    print_colored_message "Creating folders tree in root" green
+    mkdir ./$PROJECT_NAME/src/presentation/
+    mkdir ./$PROJECT_NAME/src/presentation/pages
+    mkdir ./$PROJECT_NAME/src/presentation/layouts
+    mkdir ./$PROJECT_NAME/src/presentation/components
+    mkdir ./$PROJECT_NAME/src/presentation/styles
+    mkdir ./$PROJECT_NAME/src/presentation/styles/utilities
+    mkdir ./$PROJECT_NAME/src/presentation/styles/base
+    mkdir ./$PROJECT_NAME/src/presentation/styles/pages
+    mkdir ./$PROJECT_NAME/src/presentation/styles/layouts
+    mkdir ./$PROJECT_NAME/src/presentation/styles/components
+    print_colored_message "Ended creating folders tree in root" green
 }
 
 main() {
     print_colored_message "Starting to create project" green
     generate_project
     install_dependencies
+    create_root_dir_front
     move_styles
     print_colored_message "Ended creating project" green
 }
